fa6bbb2499b37226d9a6d4f418dd27855b3d31b2
김수환 <zzanggoon8@hiconsy.com>
2022-10-27 18:31:10 +0900

    상영, Screening Manual

---
+++ src/main/java/me/ssu/objectorientedmoviereservationsystem/domains/step01/movie/Movie.java
@@ -0,0 +1,6 @@
+package me.ssu.objectorientedmoviereservationsystem.domains.step01.movie;
+
+// 영화(Movie)
+public class Movie {
+
+}
--- src/main/java/me/ssu/objectorientedmoviereservationsystem/manual/detail/ScreeningManual.java
+++ src/main/java/me/ssu/objectorientedmoviereservationsystem/manual/detail/ScreeningManual.java
@@ -10,9 +10,14 @@ public class ScreeningManual {
 		순번(sequence)
 
 	1-2) 메서드
-		1-2)-1. 상영 시작 시간을 반환하는 메서드, getStartTime()
-		1-2)-2. 순번 일치 여부 검사하는 메서드, isSequence
-		1-3)-3. 기본 요금을 반환하는 메서드, getMovieFee()
+		1-2)-1. 상영 시작 시간을 반환, getStartTime()
+		1-2)-2. 순번 일치 여부 검사, isSequence()
+		1-3)-3. 기본 요금을 반환, getMovieFee()
+		1-3)-4. 영화 예매, reserve(예매자, 인원수)
+			-> 영화 예매를 리턴한다.
+		1-3)-5. 예매 요금 금액, calculateFee(인원수)
+			-> 전쳬 예매 요금을 구하기 위해 calculateMovieFee 메서드의 반환 값에
+			-> 인원수인 audienceCount
 
 	1-3) 생성자
 		영화 상영(영화, 순번, 상영 시간)
