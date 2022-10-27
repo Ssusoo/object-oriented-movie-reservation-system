c115ae23be9b8010796c226293d3539b854e28ee
김수환 <zzanggoon8@hiconsy.com>
2022-10-27 17:17:00 +0900

    etc

---
+++ src/main/java/me/ssu/objectorientedmoviereservationsystem/ObjectOrientedMovieReservationSystemApplication.java
@@ -0,0 +1,13 @@
+package me.ssu.objectorientedmoviereservationsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ObjectOrientedMovieReservationSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ObjectOrientedMovieReservationSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/me/ssu/objectorientedmoviereservationsystem/ObjectOrientedMovieReservationSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package me.ssu.objectorientedmoviereservationsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ObjectOrientedMovieReservationSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
