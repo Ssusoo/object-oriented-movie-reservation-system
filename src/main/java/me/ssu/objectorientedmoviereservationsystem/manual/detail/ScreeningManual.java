ac2b300ed3417368a36f44696f8ded1125e3890d
김수환 <zzanggoon8@hiconsy.com>
2022-10-27 17:56:10 +0900

    상영, Screening Manual

---
+++ src/main/java/me/ssu/objectorientedmoviereservationsystem/manual/detail/ScreeningManual.java
@@ -0,0 +1,24 @@
+package me.ssu.objectorientedmoviereservationsystem.manual.detail;
+
+public class ScreeningManual {
+/*
+1. 상영(Screening)
+	: 실제로 관객들이 영화를 관람하는 사건
+	1-1) 변수
+		영화(movie)
+		상영시간(whenScreened)
+		순번(sequence)
+
+	1-2) 메서드
+		1-2)-1. 상영 시작 시간을 반환하는 메서드, getStartTime()
+		1-2)-2. 순번 일치 여부 검사하는 메서드, isSequence
+		1-3)-3. 기본 요금을 반환하는 메서드, getMovieFee()
+
+	1-3) 생성자
+		영화 상영(영화, 순번, 상영 시간)
+
+TODO 클래스를 구현하거나 다른 개발자의 클래스를 사용할 때 중요한 것은 ‘클래스의 경계’다.
+ 클래스는 내부와 외부로 구분되고 훌륭한 클래스를 설계하기 위한 핵심은 어떤 부분을 외부에 공개하고
+ 내부로 어떤 내용을 감출지 결정하는 것이다.
+*/
+}
