7930414b803718d033fd2d2032af1bfaea38798b
김수환 <zzanggoon8@hiconsy.com>
2022-10-27 20:05:36 +0900

    영화, MovieManual

---
+++ src/main/java/me/ssu/objectorientedmoviereservationsystem/manual/detail/MovieManual.java
@@ -0,0 +1,23 @@
+package me.ssu.objectorientedmoviereservationsystem.manual.detail;
+
+public class MovieManual {
+/*
+1. 영화(Movie)
+	: 영화의 기본 정보 제공
+		제목, 가격, 상영시간, 가격 정보 등
+
+	1-1) 변수
+		1-1)-1. 영화제목
+		1-1)-2. 상영시간
+		1-1)-3. 요금
+		1-1)-4. 할인정책
+
+	1-2) 생성자
+
+	1-3) 메서드
+		1-3)-1. 기본 요금 정보를 가져오기
+			getFee()
+		1-3)-2. 할인 요금 차감하기
+			calculateMovieFee()
+*/
+}
