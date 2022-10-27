aae39c58b401a33298762cbf71457974a11a2ab1
김수환 <zzanggoon8@hiconsy.com>
2022-10-27 17:39:08 +0900

    영화 예매 시스템, Structure modified

--- src/main/java/me/ssu/objectorientedmoviereservationsystem/manual/ProgramStructure.java
+++ src/main/java/me/ssu/objectorientedmoviereservationsystem/manual/ProgramStructure.java
@@ -2,19 +2,19 @@ package me.ssu.objectorientedmoviereservationsystem.manual;
 
 public class ProgramStructure {
 /* TODO 영화 예매 시스템 프로그램 구조
-1. 상영
+1. 상영(Screening)
 	1-1) 예매
 
-2. 영화
+2. 영화(Movie)
 
-3. 할인 정책
-	3-1) 금액 할인 정책
+3. 할인 정책(DiscountPolicy)
+	3-1) 금액 할인 정책(Amount Discount Policy)
 
-	3-2) 비율 할인 정책
+	3-2) 비율 할인 정책(Percent Discount Policy)
 
-4. 할인 조건
-	4-1) 순번 조건
+4. 할인 조건(Discount Condition)
+	4-1) 순번 조건(Sequence Condition)
 
-	4-2) 기간 조건
+	4-2) 기간 조건(Period Condition)
 */
 }
